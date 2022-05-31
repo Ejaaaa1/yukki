1bf739873b442998610941709dd30c010249f6aa
Ejaaa <97424874+Ejaaaa1@users.noreply.github.com>
2022-05-31 08:24:36 +0800

    Update inlinequery.py

--- YukkiMusic/utils/inlinequery.py
+++ YukkiMusic/utils/inlinequery.py
@@ -41,31 +41,32 @@ answer.extend(
         InlineQueryResultArticle(
             title="Skip Stream",
             description=f"Skip to next track. | For Specific track number: /skip [number] ",
-            thumb_url="https://telegra.ph/file/98b88e52bc625903c7a2f.png",
+            thumb_url="https://telegra.ph/Skip-05-31",
             input_message_content=InputTextMessageContent("/skip"),
         ),
         InlineQueryResultArticle(
             title="End Stream",
             description="Stop the ongoing playout on group call.",
-            thumb_url="https://telegra.ph/file/d2eb03211baaba8838cc4.png",
+            thumb_url="https://telegra.ph/Stop-05-31",
             input_message_content=InputTextMessageContent("/stop"),
         ),
         InlineQueryResultArticle(
             title="Shuffle Stream",
             description="Shuffle the queued tracks list.",
-            thumb_url="https://telegra.ph/file/7f6aac5c6e27d41a4a269.png",
+            thumb_url="https://telegra.ph/Shuffle-05-31",
             input_message_content=InputTextMessageContent("/shuffle"),
         ),
         InlineQueryResultArticle(
             title="Seek Stream",
             description="Seek the ongoing stream to a specific duration.",
-            thumb_url="https://telegra.ph/file/cd25ec6f046aa8003cfee.png",
+            thumb_url="https://telegra.ph/Loop-05-30-2",
             input_message_content=InputTextMessageContent("/seek 10"),
         ),
         InlineQueryResultArticle(
             title="Loop Stream",
             description="Loop the current playing music. | Usage: /loop [enable|disable]",
-            thumb_url="https://telegra.ph/file/081c20ce2074ea3e9b952.png",
+            thumb_url="https://telegra.ph/Loop-05-31
+",
             input_message_content=InputTextMessageContent("/loop 3"),
         ),
     ]
