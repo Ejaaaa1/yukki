0e115cc15519a71d5207cce23ec728814f2854e2
Ejaaa <97424874+Ejaaaa1@users.noreply.github.com>
2022-06-01 00:33:55 +0800

    Update seeker.py

--- YukkiMusic/plugins/misc/seeker.py
+++ YukkiMusic/plugins/misc/seeker.py
@@ -19,7 +19,6 @@ from YukkiMusic.utils.formatters import seconds_to_min
 from YukkiMusic.utils.inline import (stream_markup_timer,
                                      telegram_markup_timer)
 
-from ..admins.callback import wrong
 
 checker = {}
 
